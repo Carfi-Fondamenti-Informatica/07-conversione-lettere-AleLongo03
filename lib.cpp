3985656d8a26bdc56981aa574980ce5168710f8c
AleLongo03 <116566037+AleLongo03@users.noreply.github.com>
2022-12-08 15:59:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool funzione (char &a){
+    if ((a>=65 & a<=90) or (a>=97 & a<=122)){
+        if (a>= 65 & a<=95){
+            a+=32;
+        }else {
+            a-=32;
+        }
+        return 1;
+    }else {
+        return 0;
+    }
+}
